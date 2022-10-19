4255f98b3fca371a104041426d8bd652d6aafda2
Isaac Tay <IsaacTay@pm.me>
2022-10-19 13:27:26 +0800

    feat: :lipstick: new logo
    new logo generate by min-dalle

---
+++ docs/_app/immutable/chunks/1-7d2a188a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-7590042f.js";export{t as component};
--- docs/_app/immutable/chunks/1-cbc3491b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-68a8e37e.js";export{t as component};
